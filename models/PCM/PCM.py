abe7fe0df6019aca3a929c22dcace5ae88674464
Kaushal <kaushalgumpula@gmail.com>
2024-04-21 23:14:03 -0400

    I'm sorry for this commit, I will change it later

--- models/PCM/PCM.py
+++ models/PCM/PCM.py
@@ -222,11 +222,73 @@ class PointMambaEncoder(nn.Module):
 
         pos_proj_idx = 0
         mamba_layer_idx = 0
-        x_list = []
-        for j in range(len(self.mamba_layers_orders)):
-            mamba_layer_idx = j
-            for i in range(self.stages):
+        if self.parallel_multihead:
+            x_list = []
+            for j in range(len(self.mamba_layers_orders)):
+                mamba_layer_idx = j
+                for i in range(self.stages):
 
+                    # GAM forward
+                    p, x, x_res = self.local_grouper_list[i](p, x.permute(0, 2, 1), x_res)  # [b,g,3]  [b,g,k,d]
+                    x = self.pre_blocks_list[i](x)  # [b,d,g]
+
+                    x = x.permute(0, 2, 1).contiguous()
+                    if not self.block_residual:
+                        x_res = None
+                    x_res = self.residual_proj_blocks_list[i](x_res)
+                    # mamba forward
+                    for layer in self.mamba_blocks_list[i]:
+                        p, x, x_res = self.serialization_func(p, x, x_res, self.mamba_layers_orders[mamba_layer_idx])
+                        if self.use_windows:
+                            p, x, x_res, n_windows, p_base = self.pre_split_windows(
+                                p, x, x_res, windows_size=self.windows_size[i])
+                        if self.mamba_pos:
+                            if self.pos_type == 'share':
+                                if self.block_pos_share:
+                                    x = x + self.pos_proj(p)
+                                else:
+                                    x = x + self.pos_proj[i](p)
+                            else:
+                                x = x + self.pos_proj[pos_proj_idx](p)
+                                pos_proj_idx += 1
+                        if self.use_order_prompt:
+                            layer_order_prompt_indexes = self.per_layer_prompt_indexe[mamba_layer_idx]
+                            layer_order_prompt = self.order_prompt.weight[
+                                                layer_order_prompt_indexes[0]: layer_order_prompt_indexes[1]]
+                            layer_order_prompt = self.order_prompt_proj[i](layer_order_prompt)
+                            layer_order_prompt = layer_order_prompt.unsqueeze(0).repeat(p.shape[0], 1, 1)
+                            x = torch.cat([layer_order_prompt, x, layer_order_prompt], dim=1)
+                            if x_res is not None:
+                                x_res = torch.cat([layer_order_prompt, x_res, layer_order_prompt], dim=1)
+                            x, x_res = layer(x, x_res)
+                            x = x[:, self.promot_num_per_order:-self.promot_num_per_order]
+                            x_res = x_res[:, self.promot_num_per_order:-self.promot_num_per_order]
+                        else:
+                            x, x_res = layer(x, x_res)
+                        if self.use_windows:
+                            p, x, x_res = self.post_split_windows(p, x, x_res, n_windows, p_base)
+                        mamba_layer_idx += 1
+                    
+                    x = x.permute(0, 2, 1).contiguous()
+                    # in PCM, this is only a nn.Identity
+                    x = self.pos_blocks_list[i](x)  # [b,d,g]
+                x_list.append(x)
+
+            # bad? change later
+            for i, x in enumerate(x_list):
+                if self.cls_pooling == "max":
+                    x_list[i] = F.adaptive_max_pool1d(x, 1).squeeze(dim=-1)
+                elif self.cls_pooling == "mean":
+                    x_list[i] = x.mean(dim=-1, keepdim=False)
+                else:
+                    x_max = F.adaptive_max_pool1d(x, 1).squeeze(dim=-1)
+                    x_mean = x.mean(dim=-1, keepdim=False)
+                    x_list[i] = x_max + x_mean
+
+            x = torch.cat(x_list, dim=-1)
+            x = self.proj(x)
+        else:
+            for i in range(self.stages):
                 # GAM forward
                 p, x, x_res = self.local_grouper_list[i](p, x.permute(0, 2, 1), x_res)  # [b,g,3]  [b,g,k,d]
                 x = self.pre_blocks_list[i](x)  # [b,d,g]
@@ -267,25 +329,19 @@ class PointMambaEncoder(nn.Module):
                     if self.use_windows:
                         p, x, x_res = self.post_split_windows(p, x, x_res, n_windows, p_base)
                     mamba_layer_idx += 1
-                
                 x = x.permute(0, 2, 1).contiguous()
+
                 # in PCM, this is only a nn.Identity
                 x = self.pos_blocks_list[i](x)  # [b,d,g]
-            x_list.append(x)
 
-        # bad? change later
-        for i, x in enumerate(x_list):
             if self.cls_pooling == "max":
-                x_list[i] = F.adaptive_max_pool1d(x, 1).squeeze(dim=-1)
+                x = F.adaptive_max_pool1d(x, 1).squeeze(dim=-1)
             elif self.cls_pooling == "mean":
-                x_list[i] = x.mean(dim=-1, keepdim=False)
+                x = x.mean(dim=-1, keepdim=False)
             else:
                 x_max = F.adaptive_max_pool1d(x, 1).squeeze(dim=-1)
                 x_mean = x.mean(dim=-1, keepdim=False)
-                x_list[i] = x_max + x_mean
-
-        x = torch.cat(x_list, dim=-1)
-        x = self.proj(x)
+                x = x_max + x_mean
         return x
 
     def forward_seg_feat(self, p, x=None):
