a3b0ccd9d25875c66d4ceaae5f9bba84e016353a
Hemit Shah <hemit@cmu.edu>
2024-05-01 15:47:25 -0400

    seg forward

--- models/PCM/PCM.py
+++ models/PCM/PCM.py
@@ -327,37 +327,51 @@ class PointMambaEncoder(nn.Module):
             if not self.block_residual:
                 x_res = None
             x_res = self.residual_proj_blocks_list[i](x_res)
-            for layer in self.mamba_blocks_list[i]:
-                p, x, x_res = self.serialization_func(p, x, x_res, self.mamba_layers_orders[mamba_layer_idx])
-                if self.use_windows:
-                    p, x, x_res, n_windows, p_base, p_std = self.pre_split_windows(
-                        p, x, x_res, windows_size=self.windows_size[i])
-                if self.mamba_pos:
-                    if self.pos_type == 'share':
-                        if self.block_pos_share:
-                            x = x + self.pos_proj(p)
-                        else:
-                            x = x + self.pos_proj[i](p)
-                    else:
-                        x = x + self.pos_proj[pos_proj_idx](p)
-                        pos_proj_idx += 1
-                if self.use_order_prompt:
-                    layer_order_prompt_indexes = self.per_layer_prompt_indexe[mamba_layer_idx]
-                    layer_order_prompt = self.order_prompt.weight[
-                                         layer_order_prompt_indexes[0]: layer_order_prompt_indexes[1]]
-                    layer_order_prompt = self.order_prompt_proj[i](layer_order_prompt)
-                    layer_order_prompt = layer_order_prompt.unsqueeze(0).repeat(p.shape[0], 1, 1)
-                    x = torch.cat([layer_order_prompt, x, layer_order_prompt], dim=1)
-                    if x_res is not None:
-                        x_res = torch.cat([layer_order_prompt, x_res, layer_order_prompt], dim=1)
+            # mamba forward
+            if self.parallel_multihead: 
+                outs = []
+                for layer in self.mamba_blocks_list[i]:
+                    p, x, x_res = self.serialization_func(p, x, x_res, self.mamba_layers_orders[mamba_layer_idx])
                     x, x_res = layer(x, x_res)
-                    x = x[:, self.promot_num_per_order:-self.promot_num_per_order]
-                    x_res = x_res[:, self.promot_num_per_order:-self.promot_num_per_order]
-                else:
+                    outs.append(x)
+                    mamba_layer_idx += 1
+                x = torch.cat(outs, dim=-1)
+                x = self.multihead_projection[i](x)
+            else: 
+                for layer in self.mamba_blocks_list[i]:
+                    p, x, x_res = self.serialization_func(p, x, x_res, self.mamba_layers_orders[mamba_layer_idx])
                     x, x_res = layer(x, x_res)
-                if self.use_windows:
-                    p, x, x_res = self.post_split_windows(p, x, x_res, n_windows, p_base, p_std)
-                mamba_layer_idx += 1
+                    mamba_layer_idx += 1
+            
+                # if self.use_windows:
+                #     p, x, x_res, n_windows, p_base, p_std = self.pre_split_windows(
+                #         p, x, x_res, windows_size=self.windows_size[i])
+                # if self.mamba_pos:
+                #     if self.pos_type == 'share':
+                #         if self.block_pos_share:
+                #             x = x + self.pos_proj(p)
+                #         else:
+                #             x = x + self.pos_proj[i](p)
+                #     else:
+                #         x = x + self.pos_proj[pos_proj_idx](p)
+                #         pos_proj_idx += 1
+                # if self.use_order_prompt:
+                #     layer_order_prompt_indexes = self.per_layer_prompt_indexe[mamba_layer_idx]
+                #     layer_order_prompt = self.order_prompt.weight[
+                #                          layer_order_prompt_indexes[0]: layer_order_prompt_indexes[1]]
+                #     layer_order_prompt = self.order_prompt_proj[i](layer_order_prompt)
+                #     layer_order_prompt = layer_order_prompt.unsqueeze(0).repeat(p.shape[0], 1, 1)
+                #     x = torch.cat([layer_order_prompt, x, layer_order_prompt], dim=1)
+                #     if x_res is not None:
+                #         x_res = torch.cat([layer_order_prompt, x_res, layer_order_prompt], dim=1)
+                #     x, x_res = layer(x, x_res)
+                #     x = x[:, self.promot_num_per_order:-self.promot_num_per_order]
+                #     x_res = x_res[:, self.promot_num_per_order:-self.promot_num_per_order]
+                # else:
+                #     x, x_res = layer(x, x_res)
+                # if self.use_windows:
+                #     p, x, x_res = self.post_split_windows(p, x, x_res, n_windows, p_base, p_std)
+            
             x = x.permute(0, 2, 1).contiguous()
             x = self.pos_blocks_list[i](x)  # [b,d,g]
 
